4e9f64f05654c054ad56ec1e96dae8274e3e2853
hyup <ariz1623@g.kmou.ac.kr>
2021-01-21 23:00:58 +0900

    Update my code

--- morsecode.py
+++ morsecode.py
@@ -179,7 +179,7 @@ def get_cleaned_english_sentence(raw_english_sentence):
     chars = '.,!?'
     for char in chars:
         raw_english_sentence = raw_english_sentence.replace(char,'')
-    return raw_english_sentence
+    return raw_english_sentence.strip()
     # ==================================
 
 
@@ -240,10 +240,8 @@ def encoding_character(english_character):
     """
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
-    
     morse_code_dict = get_morse_code_dict()
     
-
     if english_character in morse_code_dict:
         return morse_code_dict[english_character]
     
